2215cec53fc026c77bcd41b1568e2061a82f15dc
Amirgali <aturalinov@yandex.ru>
2021-10-03 10:14:23 +0300

    First commit

---
+++ SchoolStore/Classes/Business/Factories/CoreFactory.swift
@@ -0,0 +1,17 @@
+// \HxH School iOS Pass
+// Copyright © 2021 Heads and Hands. All rights reserved.
+//
+
+import Foundation
+
+enum CoreFactory {
+    static let requestBuilder: RequestBuilder = RequestBuilderImpl(dataService: Self.dataService)
+
+    static let networkProvider: NetworkProvider = NetworkProviderImpl(requestBuilder: requestBuilder)
+
+    static let dataService: DataService = DataServiceImpl()
+
+    static func buildAuthService() -> AuthService {
+        AuthServiceImpl(networkProvider: Self.networkProvider, dataService: Self.dataService)
+    }
+}
