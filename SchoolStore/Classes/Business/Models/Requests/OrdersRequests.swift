ee41388aa149c36f30b944f11a7779fb87433dcc
Amirgali <aturalinov@yandex.ru>
2021-10-05 15:26:20 +0300

    Добавил OrdersRequests + Моки

--- SchoolStore/Classes/Business/Models/Requests/OrdersRequests.swift
+++ SchoolStore/Classes/Business/Models/Requests/OrdersRequests.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+enum OrdersRequests: Request {
+    case postOrdersCheckout(productId: String, size: String, quantity: Int, house: String, apartment: String?, etd: String)
+    case getOrders
+    case putOrdersIdCancel
+    
+    var path: String {
+        switch self {
+        case .postOrdersCheckout:
+            return "orders/checkout"
+        case .getOrders:
+            return "orders"
+        case .putOrdersIdCancel:
+            return "orders/{order_id}/cancel"
+        }
+    }
+    
+    var method: RequestMethod {
+        switch self {
+        case .postOrdersCheckout:
+            return .post
+        case .getOrders:
+            return .get
+        case .putOrdersIdCancel:
+            return .put
+        }
+    }
+    
+    var mock: Data? {
+        switch self {
+        case .postOrdersCheckout:
+            guard let path = Bundle.main.path(forResource: "postOrdersCheckout", ofType: "json"),
+                  let data = try? Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
+            else {
+                return nil
+            }
+            return data
+        case .getOrders:
+            guard let path = Bundle.main.path(forResource: "getOrders", ofType: "json"),
+                  let data = try? Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
+            else {
+                return nil
+            }
+            return data
+        case .putOrdersIdCancel:
+            guard let path = Bundle.main.path(forResource: "putOrdersIdCancel", ofType: "json"),
+                  let data = try? Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
+            else {
+                return nil
+            }
+            return data
+        }
+    }
+}
