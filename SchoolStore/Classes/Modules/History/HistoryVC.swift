73414d142139ff8889bf97e451f51d9d61a32ced
Amirgali <aturalinov@yandex.ru>
2021-10-13 17:38:00 +0300

    Add segment controll

--- SchoolStore/Classes/Modules/History/HistoryVC.swift
+++ SchoolStore/Classes/Modules/History/HistoryVC.swift
@@ -6,6 +6,43 @@ import UIKit
 
 final class HistoryVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    var segmentedControll: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let titles = ["One", "Two"]
+        segmentedControll = UISegmentedControl(items: titles)
+        
+        segmentedControll.setTitleTextAttributes(
+            [NSAttributedString.Key.font: UIFont(name: "Roboto", size: 14) ?? "Arial",
+             NSAttributedString.Key.foregroundColor: UIColor(red: 255, green: 255, blue: 255, alpha: 0.5)]
+            , for: .normal)
+        
+        segmentedControll.setTitleTextAttributes(
+            [NSAttributedString.Key.font: UIFont(name: "Roboto", size: 14) ?? "Arial",
+             NSAttributedString.Key.foregroundColor: UIColor(red: 255, green: 255, blue: 255, alpha: 1)]
+            , for: .selected)
+        
+        segmentedControll.backgroundColor = UIColor.blue
+        segmentedControll.layer.borderColor = UIColor.blue.cgColor
+        segmentedControll.selectedSegmentTintColor = UIColor.blue
+        segmentedControll.layer.cornerRadius = 0
+        segmentedControll.selectedSegmentIndex = 0
+        segmentedControll.contentMode = .scaleAspectFill
+
+        view.addSubview(segmentedControll)
+        segmentedControll.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            segmentedControll.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            segmentedControll.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            segmentedControll.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            segmentedControll.heightAnchor.constraint(equalToConstant: 48)
+        ])
+        
+        
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
         return 1
@@ -42,3 +79,4 @@ final class HistoryVC: UIViewController, UITableViewDataSource, UITableViewDeleg
         return UISwipeActionsConfiguration(actions: [deleteAction])
     }
 }
+
